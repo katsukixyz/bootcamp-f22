d192edc00de612860c70b8c21e2cef0b331b22e7
Katsuki Chan <katsukidotxyz@gmail.com>
2022-09-22 18:07:40 -0400

    reverted lec3

--- lec4/demo/variables.js
+++ lec4/demo/variables.js
@@ -1,17 +1,35 @@
-function let_example() {
-  if (true) {
-    let a = "bog";
-  }
-  return a;
-}
+// function let_example() {
+//   if (true) {
+//     let a = "bog";
+//   }
+//   return a;
+// }
 
-console.log(let_example());
+// console.log(let_example());
 
-function var_example() {
-  if (true) {
-    var b = "bog";
-  }
-  return b;
-}
+// function var_example() {
+//   if (true) {
+//     var b = "bog";
+//   }
+//   return b;
+// }
 
-console.log(var_example());
+// console.log(var_example());
+// [{ 0: 8 }, { 1: 10 }];
+
+const arr = [8, 10, 22, 73, 45, 25];
+// console.log(
+//   arr.map((value, index) => {
+//     const obj = {};
+//     obj[index] = value;
+//     return obj;
+//   })
+// );
+
+const final = arr.reduce((prev, cur, index) => {
+  // const obj = prev;
+  prev[cur] = index;
+  return prev;
+}, {});
+
+console.log(final);
